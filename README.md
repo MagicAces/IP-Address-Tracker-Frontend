0801a510bfc5d1849b30a5df3cce71806349f4a9
MagicAces <isaac.oniti14@gmail.com>
2022-11-21 12:41:04 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![](./screenshot.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Desktop-first workflow
+
+
+### What I learned
+
+I learnt how to use CSS Flexbox for a responsive layout.
+
+```js
+function checkIfValidIP(str) {
+  // Regular expression to check if string is a IP address
+  const regexExp = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+
+  return regexExp.test(str);
+}
+```
+
+
+## Author
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/MagicAces)
+- Twitter - [@Isaac Oniti](https://www.twitter.com/yourusername)
---
+++ map.js
@@ -0,0 +1,69 @@
+
+function checkIfValidIP(str) {
+  // Regular expression to check if string is a IP address
+  const regexExp = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+
+  return regexExp.test(str);
+}
+
+const url = "https://geo.ipify.org/api/v1/country,city?apiKey=";
+const api_key = "at_lIXUhB91chUFlaH8IaQfS3nuQyPGb";
+let data = {
+	apiKey: api_key,
+	ipAddress: ''
+}
+let details = [];
+
+var map = L.map('map');
+
+const form = document.querySelector("form");
+
+
+function displayIP(ip) {
+	$(function () {
+	   $.ajax({
+	       url: "https://geo.ipify.org/api/v1",
+	       data: data,
+	       success: function(data) {
+	           $(".ip-box h2").text(data.ip);
+						 $(".loc-box h2").text(data.location.city + ", " + data.location.region + ", " + data.location.country);
+						 $(".time-box h2").text("UTC " + data.location.timezone);
+						 $(".isp-box h2").text(data.isp);
+
+						 map.setView([data.location.lat, data.location.lng], 16);
+
+						 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+							 maxZoom: 24,
+							 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+						 }).addTo(map);
+
+						 const icon = L.icon({
+						   iconUrl: "images/icon-location.svg",
+						 });
+
+						 const marker = L.marker([data.location.lat, data.location.lng], {icon: icon}).addTo(map);
+
+	       }
+	   });
+	 });
+
+  displayIP("");
+
+  $("input").click(()=> {
+    $("input").css("border", "none");
+  });
+
+  $("form").on("submit", function(event) {
+  	event.preventDefault();
+  	let ip = form.elements["ip"].value;
+    console.log(ip);
+  	if(checkIfValidIP(ip))
+  		data.ipAddress = ip;
+    else {
+      $("input").blur();
+      $("input").css("border", "3px solid red");
+    }
+  	displayIP(ip);
+    $("input").val("");
+	});
+});
